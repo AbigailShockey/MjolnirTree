8aa304b598871e6d9e6c4274a7ef7b2c7e0ce3e3
Abigail Shockey <abbashockey@gmail.com>
2019-11-26 11:37:50 -0600

    Added bootstrap suppot fxn and fixed paths

--- app/calculateHammingDistanceTree.py
+++ app/calculateHammingDistanceTree.py
@@ -11,26 +11,46 @@ def checkexists(path):
     else:
         return True
         
-def hammingDistanceTree(tsvfile, out, transpose, boot):
-    treespath = os.path.join(out,"trees")
-    checkexists(treespath)
-    # setup command
-    cmd = f'bash -c \"hammingDistanceTree.py {tsv} {transpose} {boot}\"'
-    # denote logs
-    with open(logfile,'a') as outlog:
-        outlog.write('***********\n')
-        outlog.write(f'Calculating hamming distance tree and boostrapping {boot} times\n')
-        results = cd.call('ashockey/mjolnir:latest',cmd,'/data',{out:"/data",treespath:"/output"})
-        outlog.write('***********\n')
+def hammingDistanceTree(tsvfile, out, prefix, transpose, boot):
+  inputpath = tsvfile
+  treespath = os.path.join(out,"trees")
+  checkexists(treespath)
 
-def consensusTree(out)
+  # setup command
+  cmd = f'bash -c \"hammingDistanceTree.py {inputpath} /output/ {transpose} {boot}\"'
+
+  # denote logs
+  with open(logfile,'a') as outlog:
+      outlog.write('***********\n')
+      outlog.write(f'Calculating hamming distance tree and boostrapping {boot} times\n')
+      results = cd.call('ashockey/mjolnir:latest',cmd,'/data',{inputpath:"/data",treespath:"/output"})
+      outlog.write('***********\n')
+
+def consensusTree(out):
   inputpath = os.path.join(out,"trees")
   consensuspath = os.path.join(out,"consensus")
   checkexists(consensuspath)
+
   # setup command
-  cmd = f'bash -c \"sumtrees.py -s consensus -o phylo-mle-support.sumtrees -f0.95 --p --d0 {inputpath}/bootstrapped_nj_trees.newick\"'
+  cmd = f'bash -c \"sumtrees.py -s consensus -o /output/mrc95.tree -f0.95 --p --d0 /data/bootstrapped_nj_trees.newick\"'
+
+  # denote logs
   with open(logfile,'a') as outlog:
       outlog.write('***********\n')
-      outlog.write('Calculating consensus tree\n')
-      results = cd.call('ashockey/mjolnir:latest',cmd,'/data',{input_path:"/data",consensuspath:"/output"})
+      outlog.write('Calculating 95% majority rule consensus tree\n')
+      results = cd.call('ashockey/mjolnir:latest',cmd,'/data',{inputpath:"/data",consensuspath:"/output"})
+      outlog.write('***********\n')
+
+def boostrapSupport(out)
+  inputtrees = os.path.join(out,"trees")
+  inputconsensus = os.path.join(out,"consensus")
+  supportpath = os.path.join(out,"bootstrapSupport")
+  checkexists(consensuspath)
+
+  # setup command
+  cmd = f'bash -c \"sumtrees.py -d0 -p -o /output/mrc95_boostrapSupport.tree -t /data/mrc95.tree /data/bootstrapped_nj_trees.newick\"'
+  with open(logfile,'a') as outlog:
+      outlog.write('***********\n')
+      outlog.write('Calculating support for nodes in the consensus tree\n')
+      results = cd.call('ashockey/mjolnir:latest',cmd,'/data',{inputtrees:inputconsensus:"/data",supportpath:"/output"})
       outlog.write('***********\n')
