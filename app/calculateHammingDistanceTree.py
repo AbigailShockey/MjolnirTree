d162f92dc9855c1e606b094de0834e34634ee5d7
Abigail Shockey <abbashockey@gmail.com>
2019-12-03 16:43:53 -0600

    Updated paths and fixed var issues

--- app/calculateHammingDistanceTree.py
+++ app/calculateHammingDistanceTree.py
@@ -2,6 +2,8 @@
 
 import os,sys
 import app.callDocker as cd
+import subprocess as sub
+import shlex
 
 def checkexists(path):
     path = os.path.abspath(path)
@@ -10,29 +12,37 @@ def checkexists(path):
         return False
     else:
         return True
-        
-def hammingDistanceTree(tsvfile, out, prefix, transpose, boot):
-  inputpath = tsvfile
-  treespath = os.path.join(out,"trees")
+
+def hammingDistanceTree(tsvfile, out, transpose, boot):
+  logfile = os.path.join(out,'hdt.log')
+  inputpath = os.path.dirname(tsvfile)
+  tsv = os.path.basename(tsvfile)
+  treespath = os.path.join(out,"distanceTrees")
   checkexists(treespath)
 
   # setup command
-  cmd = f'bash -c \"hammingDistanceTree.py {inputpath} /output/ {transpose} {boot}\"'
-
+  cmd = f'bash -c \"mjlonir_test.py /data/{tsv} /output/ {boot} {transpose}\"'
+  print(cmd)
   # denote logs
   with open(logfile,'a') as outlog:
       outlog.write('***********\n')
       outlog.write(f'Calculating hamming distance tree and boostrapping {boot} times\n')
       results = cd.call('ashockey/mjolnir:latest',cmd,'/data',{inputpath:"/data",treespath:"/output"})
       outlog.write('***********\n')
+  cmd = shlex.split(f"cp {treespath}/bootstrapped_nj_trees.newick {out}")
+  sub.Popen(cmd).wait()
+  cmd = shlex.split(f"cp {treespath}/hamming_distance_matrix.tsv {out}")
+  sub.Popen(cmd).wait()
+
 
 def consensusTree(out):
-  inputpath = os.path.join(out,"trees")
-  consensuspath = os.path.join(out,"consensus")
+  logfile = os.path.join(out,'consensus.log')
+  inputpath = os.path.join(out,"distanceTrees")
+  consensuspath = os.path.join(out,"consensusTree")
   checkexists(consensuspath)
 
   # setup command
-  cmd = f'bash -c \"sumtrees.py -s consensus -o /output/mrc95.tree -f0.95 --p --d0 /data/bootstrapped_nj_trees.newick\"'
+  cmd = f'bash -c \"sumtrees.py -s consensus -o /output/mrc95.tree -f0.95 --percentages --decimals=0 /data/bootstrapped_nj_trees.newick\"'
 
   # denote logs
   with open(logfile,'a') as outlog:
@@ -40,21 +50,24 @@ def consensusTree(out):
       outlog.write('Calculating 95% majority rule consensus tree\n')
       results = cd.call('ashockey/mjolnir:latest',cmd,'/data',{inputpath:"/data",consensuspath:"/output"})
       outlog.write('***********\n')
+  cmd = shlex.split(f"cp {consensuspath}/mrc95.tree {out}")
+  sub.Popen(cmd).wait()
 
-def boostrapSupport(out)
-  inputtrees = os.path.join(out,"trees")
-  inputconsensus = os.path.join(out,"consensus")
+def boostrapSupport(out):
+  logfile = os.path.join(out,'support.log')
   supportpath = os.path.join(out,"bootstrapSupport")
   checkexists(supportpath)
 
   # setup command
-  cmd = f'bash -c \"sumtrees.py -d0 -p -o /output/mrc95_boostrapSupport.tree -t /data/mrc95.tree /data/bootstrapped_nj_trees.newick\"'
+  cmd = f'bash -c \"sumtrees.py --decimals=0 -p -o /output/mrc95_boostrapSupport.tree -t /data/mrc95.tree /data/bootstrapped_nj_trees.newick\"'
   with open(logfile,'a') as outlog:
       outlog.write('***********\n')
       outlog.write('Calculating support for nodes in the consensus tree\n')
-      results = cd.call('ashockey/mjolnir:latest',cmd,'/data',{inputtrees:inputconsensus:"/data",supportpath:"/output"})
+      results = cd.call('ashockey/mjolnir:latest',cmd,'/data',{out:"/data",supportpath:"/output"})
       outlog.write('***********\n')
-        
+  cmd = shlex.split(f"cp {supportpath}/mrc95_boostrapSupport.tree {out}")
+  sub.Popen(cmd).wait()
+
 # ------------------------------------------------------
 
 def calculateHammingDistanceTree(tsvfile, out, transpose, boot):
@@ -62,5 +75,3 @@ def calculateHammingDistanceTree(tsvfile, out, transpose, boot):
     if boot != 0:
         consensusTree(out)
         boostrapSupport(out)
-    if boot == 0:
-        continue
