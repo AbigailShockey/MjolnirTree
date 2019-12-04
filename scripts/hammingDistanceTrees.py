354b8a70ade0a5a9e8d2bcf4ec739b2e115be0e3
AbigailShockey <abbashockey@gmail.com>
2019-12-04 14:13:44 -0600

    Fixed app folder

--- scripts/hammingDistanceTrees.py
+++ scripts/hammingDistanceTrees.py
@@ -24,8 +24,8 @@ class ArgParser(argparse.ArgumentParser):
 parser = ArgParser(description='Calculate pairwise hamming distance matrix from gene prensece/absence matrix and create nj tree')
 parser.add_argument('tsv', type=str, help="Binary gene presence and absence file (tab delimited)")
 parser.add_argument('out', type=str, help="Output directory")
-parser.add_argument('transpose', type=str, help="Transpose tsv")
 parser.add_argument('boot', type=int, help="Perform boostrapping n times")
+parser.add_argument('transpose', type=str, help="Transpose tsv")
 
 args = parser.parse_args()
 
@@ -109,4 +109,6 @@ if args.boot != 0:
         trees.append(rnj_tree)
 
     # write all permuted nj trees to file
-    trees.write(path=os.path.join(out,"bootstrapped_nj_trees.newick"), schema="newick")
+    trees.write(
+        path=os.path.join(out,"bootstrapped_nj_trees.newick"),
+        schema="newick")
